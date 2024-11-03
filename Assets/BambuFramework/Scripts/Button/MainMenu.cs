cf805cae011c996bdf1c0a06915f636fd587b941
Coldsandwich189 <50047634+yikfungtan99@users.noreply.github.com>
2024-11-03 16:46:54 +0800

    buttons stuff

---
+++ Assets/BambuFramework/Scripts/Button/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace BambuFramework
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void Play()
+        {
+
+        }
+
+        public void Setting()
+        {
+
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/BambuFramework/Scripts/Button/SettingButton.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace BambuFramework
+{
+    public class SettingButton : MonoBehaviour
+    {
+
+    }
+}
